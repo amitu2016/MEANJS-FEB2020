c1ee777c6f591f0040304a84f9dfc25f7671bc46
Amit Upadhyay <amitu2016@gmail.com>
2020-08-21 11:00:33 +0530

    First commit

---
+++ src/1.js
@@ -0,0 +1,21 @@
+
+//Declaring a function
+function helloWorld() {
+    console.log("I am a named Function");
+}
+
+
+//calling a function
+helloWorld();
+
+let helloAgain = function() {
+    console.log("I am Anonymous function assigned to a variable");
+}
+
+helloAgain();
+
+let helloArrow = ()=> {
+    console.log("I am a arrow function");
+}
+
+helloArrow();
---
+++ src/hello.js
@@ -0,0 +1,4 @@
+console.log('Hello World');
+
+console.log(1, "ONE");
+console.log(2, "TWO");
